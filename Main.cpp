497bdc0c0ae0f1b836d89c5b368ade263d372958
Ardikaas <ardikaajisetiawan@gmail.com>
2023-04-01 22:11:41 +0700

    menambahkan void initial dan void input

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,24 @@ node* curr;
 node* entry;
 node* del;
 
+void initial(){
+  head = NULL;
+  tail = NULL;
+}
+
+void input(int dt){
+  entry = (node* )malloc(sizeof(node));
+  entry -> data = dt;
+  entry -> next = NULL;
+  if (head == NULL){
+    head = entry;
+    tail = head;
+  }else{
+    tail -> next = entry;
+    tail = entry;
+  }
+}
+
 int main(int argc, char const *argv[])
 {
   
